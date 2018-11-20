94b90c80585d2b77bad810dba980e45489f5ee1b
aiuradun <aiuradun@gmail.com>
2018-11-20 01:13:18 +0100

    somewhat less broken
    i still suck

--- src/app/components/add-item/add-item.component.ts
+++ src/app/components/add-item/add-item.component.ts
@@ -9,7 +9,6 @@ import { ActivatedRoute } from '@angular/router'
   styleUrls: ['./add-item.component.css']
 })
 export class AddItemComponent implements OnInit {
-  //@import() item : Item;
   item:Item = {
     title: '',
     description: ''
@@ -19,22 +18,37 @@ export class AddItemComponent implements OnInit {
 
   constructor(private itemService : ItemService,
               private route: ActivatedRoute) {
-    this.route.params.subscribe(params => console.log(params));
-    //this.route.params.subscribe(params => this.itemService.getItem(params['id']));
-    //this.route.params.subscribe(params => this.itemService.getItem(params['id']));
-    this.route.params.subscribe(params => console.log(params['id']);
-    let a:any
-    this.route.params.subscribe(params => a = params['id'];
-    console.log(a);
-    this.route.params.subscribe(params => this.itemService.getItem(params['id']).subscribe(item => {
-      this.item = item;
-    }));
+    // this.route.params.subscribe(params => console.log(params));
+
+
+    // this.route.params.subscribe(params => a = params['id']);
+ 
+    
     //    this.route.params.subscribe(params => this.itemService.getItem(params['id']));
     //this item = this.itemService.getItem(this.route.params.pipe)
    }
 
   ngOnInit() {
+/*    let id : string
+    this.route.params.subscribe(params => id = params['id']);
+    this.itemService.getItem(id).subscribe(item => {
+      this.item = item;
+      */
+    this.route.params.subscribe(params =>
+      this.itemService.getItem(params['id']).subscribe(item => {
+        this.item = item;
+        console.log(this.item.title)
+      })
+    );
+//      console.log(this.item.title, this.item.description)
+    
 
+    //this.route.params.subscribe(params => this.itemService.getItem(params['id']));
+
+    //this.itemService.getItem(this.item.id).subscribe(item => {
+      //console.log(items);
+        //this.item = item;
+      //});
   }
 
   onSubmit(){
--- src/app/services/item.service.ts
+++ src/app/services/item.service.ts
@@ -3,7 +3,6 @@ import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument 
 import { Observable, throwError } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Item } from '../models/item';
-import { R3InjectableMetadata } from '@angular/compiler';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +10,7 @@ import { R3InjectableMetadata } from '@angular/compiler';
 export class ItemService {
   itemsCollection: AngularFirestoreCollection<Item>;
   items: Observable<Item[]>;
-  item: Observable<Item>;
+  item: Item;
 
   constructor(public afs: AngularFirestore) { 
     this.itemsCollection = this.afs.collection('items', ref => ref.limit(20));
@@ -34,21 +33,8 @@ export class ItemService {
     return this.items;
   }
 
-  getItem(item: Item) {
-    this.items = this.itemsCollection.snapshotChanges()
-    .pipe(
-      map(changes => {
-          return(changes.map(a=>{
-            const data= a.payload.doc.data() as Item
-            data.id = a.payload.doc.id;
-            return data;
-
-          })
-      })
-    );
-
-    return this.item
-
+  getItem(id) {
+    return this.itemsCollection.doc(id).get()
   }
 
   addItem(item: Item) {
